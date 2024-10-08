1aca760412b04ee274133548f0d59eec0cf1f263
Ilkhomjon99 <ilhomjonalyakupov2672@gmail.com>
2024-10-08 19:33:53 +0500

    innit

---
+++ script.js
@@ -0,0 +1,29 @@
+let burgerMenu = document.querySelector('.burger-menu');
+burgerMenu.addEventListener('click', openPopUp);
+
+function openPopUp() {
+  let mob = document.querySelector('.mobile-menu');
+ mob.style.display='block';
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
