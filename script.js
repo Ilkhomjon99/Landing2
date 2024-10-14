1dabe5315d9b3da065e45b2fa6643bec904a0612
Ilkhomjon99 <ilhomjonalyakupov2672@gmail.com>
2024-10-14 17:48:26 +0500

    mylanding

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+
+
+
 let burgerMenu = document.querySelector('.burger-menu');
 burgerMenu.addEventListener('click', openPopUp);
 
 function openPopUp() {
   let mob = document.querySelector('.mobile-menu');
- mob.style.display='block';
-  
+  mob.style.display = 'block';
+}
+
+let mobileMenu = document.querySelector('.mobile-menu');
+mobileMenu.addEventListener('click', hidePopUp);
+
+function hidePopUp() {
+  let mob = document.querySelector('.mobile-menu');
+  mob.style.display = 'none';
 }
 
 
